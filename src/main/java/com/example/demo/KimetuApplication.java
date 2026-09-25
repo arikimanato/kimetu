e6c4402981d77c7cad72d9ef2770fcef83e0387d
Mai0405 <>
2026-09-25 12:00:11 +0900

    test宮本

--- src/main/java/com/example/demo/KimetuApplication.java
+++ src/main/java/com/example/demo/KimetuApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KimetuApplication {
 
+	
+	b
 	public static void main(String[] args) {
 		SpringApplication.run(KimetuApplication.class, args);
 	}
