a4046f87571860fbb9a1263363ec01fe2206b267
Mai0405 <>
2026-09-25 15:12:37 +0900

    宮本テスト

--- src/main/java/com/example/demo/KimetuApplication.java
+++ src/main/java/com/example/demo/KimetuApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KimetuApplication {
 
+	B-宮本
+	
 	public static void main(String[] args) {
 		SpringApplication.run(KimetuApplication.class, args);
 	}
